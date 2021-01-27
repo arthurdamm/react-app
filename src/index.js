e568fdccb9a7d19f7830c48afb4fca5d7e4b843a
Arthur Damm <indigoarthur@gmail.com>
2021-01-27 09:50:09 -0800

    Add first component

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+function Greeting() {
+  return <h4>a first component</h4>;
+}
+
+ReactDom.render(<Greeting/>, document.getElementById('root'));
